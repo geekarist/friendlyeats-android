fbd461d5d0948240018854743ca3466c8dca9d48
Christobal <geekarist@users.noreply.github.com>
2019-09-28 15:17:27 +0200

    Convert FirestoreAdapter to Kotlin

---
+++ app/src/main/java/com/google/firebase/example/fireeats/adapter/FirestoreAdapter.kt
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.firebase.example.fireeats.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
+import java.util.*
+
+/**
+ * RecyclerView adapter for displaying the results of a Firestore [Query].
+ *
+ * Note that this class forgoes some efficiency to gain simplicity. For example, the result of
+ * [DocumentSnapshot.toObject] is not cached so the same object may be deserialized
+ * many times as the user scrolls.
+ *
+ * See the adapter classes in FirebaseUI (https://github.com/firebase/FirebaseUI-Android/tree/master/firestore) for a
+ * more efficient implementation of a Firestore RecyclerView Adapter.
+ */
+abstract class FirestoreAdapter<VH : RecyclerView.ViewHolder>(private var mQuery: Query?) : RecyclerView.Adapter<VH>() {
+    private var mRegistration: ListenerRegistration? = null
+
+    private val mSnapshots = ArrayList<DocumentSnapshot>()
+
+    fun startListening() {
+        // TODO(developer): Implement
+    }
+
+    fun stopListening() {
+        if (mRegistration != null) {
+            mRegistration!!.remove()
+            mRegistration = null
+        }
+
+        mSnapshots.clear()
+        notifyDataSetChanged()
+    }
+
+    fun setQuery(query: Query) {
+        // Stop listening
+        stopListening()
+
+        // Clear existing data
+        mSnapshots.clear()
+        notifyDataSetChanged()
+
+        // Listen to new query
+        mQuery = query
+        startListening()
+    }
+
+    override fun getItemCount(): Int {
+        return mSnapshots.size
+    }
+
+    protected fun getSnapshot(index: Int): DocumentSnapshot {
+        return mSnapshots[index]
+    }
+
+    protected open fun onError(e: FirebaseFirestoreException) {}
+
+    protected open fun onDataChanged() {}
+
+    companion object {
+
+        private val TAG = "Firestore Adapter"
+    }
+}
